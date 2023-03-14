f789c6f2061ee3e84493e409323dfd90642098fb
Miguel Martinez <miguel@miguelmartinez.com>
2023-03-14 12:06:19 -0500

    Comma Separated List

---
+++ src/directory-compare/CommaSeparatedList.cs
@@ -0,0 +1,31 @@
+﻿namespace DirectoryCompare;
+
+public class CommaSeparatedList
+{
+    public ISet<string> List { get; } = new HashSet<string>();
+
+    public CommaSeparatedList()
+    {
+    }
+
+    public CommaSeparatedList(string patterns)
+    {
+        if (string.IsNullOrWhiteSpace(patterns) == false)
+        {
+            Initialize(patterns.Split(";"));
+        }
+    }
+
+    public CommaSeparatedList(string[] patterns)
+    {
+        Initialize(patterns);
+    }
+
+    private void Initialize(string[] patterns)
+    {
+        foreach (string pattern in patterns)
+        {
+            List.Add(pattern);
+        }
+    }
+}
